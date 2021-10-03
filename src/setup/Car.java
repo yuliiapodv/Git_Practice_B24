8b416e97aead0b999413345ff1a99565d5bed060
yuliiapodvirna <yuliia.podvirna@gmail.com>
2021-10-03 12:37:54 -0700

    made car in master branch

---
+++ src/setup/Car.java
@@ -0,0 +1,12 @@
+package setup;
+
+public class Car {
+
+    // this a car class from master branch
+
+    //we will have conflict
+
+    // because there is different text in different branches for car
+
+
+}
